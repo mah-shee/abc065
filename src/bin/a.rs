18e399a016ce338c24b91d21e8cbc99a344e4d2a
masa <masatoshi.sboard@gmail.com>
2020-09-15 17:23:21 +0900

    Done c

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,8 +1,27 @@
 #[allow(unused_imports)]
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
+use std::cmp::{max, min};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        m: usize,
+    }
+    let waru = 1_000_000_000 + 7;
+    let mut ans = 0;
+    let diff = max(n, m) - min(n, m);
+    if diff == 0 {
+        ans = 2 * factorial(n, waru) * factorial(m, waru) / waru;
+    } else if diff == 1 {
+        ans = factorial(n, waru) * factorial(m, waru) / waru;
+    }
+    println!("{}", ans);
+}
+fn factorial(a: usize, waru: usize) -> usize {
+    if a < 2 {
+        1
+    } else {
+        a / waru * factorial(a - 1, waru)
     }
 }
